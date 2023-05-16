65f61e55fb3f6ace9d2f82ecf93719e7c6f050f1
Awoken Infinity <Sergio25Andrade50@gmail.com>
2023-05-16 18:46:53 +0200

    Preparada clase básica (PFCPokeSearch) que sirve a la libreria de DaggerHilt para preparar la clase para poder user Hilt y aplicar inyección de dependencias con esta libreria

---
+++ pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/PFCPokeSearch.kt
@@ -0,0 +1,8 @@
+package com.sergio.pfcpokesearch
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PFCPokeSearch: Application() {
+}
--- pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/ui/view/MainActivity.kt
+++ pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sergio.pfcpokesearch
+package com.sergio.pfcpokesearch.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sergio.pfcpokesearch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
