b8ec64c381fbed71cad7c40df0a30a0840f9e458
Awoken Infinity <Sergio25Andrade50@gmail.com>
2023-05-17 15:12:20 +0200

    Añadidos comentarios

--- pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/domain/model/FilteredPokemon.kt
+++ pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/domain/model/FilteredPokemon.kt
@@ -2,6 +2,7 @@ package com.sergio.pfcpokesearch.domain.model
 
 import com.sergio.pfcpokesearch.data.model.*
 
+// Modelo de datos de la capa de dominio
 data class FilteredPokemon(
     var name: String,
     val species: Species,
@@ -12,5 +13,6 @@ data class FilteredPokemon(
     val weight: Int
 )
 
+// Función que convierte la info del modelo de datos de la API al de la capa de dominio
 fun FilteredPokemonModel.toDomain() = FilteredPokemon(name, species, sprites,
     stats, types, height, weight)
