f4f6682c4eacac371164fb2b63052a6532914423
Awoken Infinity <Sergio25Andrade50@gmail.com>
2023-05-21 14:21:52 +0200

    Completadas las interfaces dao para cada tabla donde se almacenan las querys para cada una de ellas

--- pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/data/database/entities/dao/FavoritesDao.kt
+++ pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/data/database/entities/dao/FavoritesDao.kt
@@ -1,4 +1,22 @@
 package com.sergio.pfcpokesearch.data.database.entities.dao
 
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
 interface FavoritesDao {
+
+    // Inserta un pokemon en la tabla
+    @Query("INSERT INTO favorites (pokemonName) VALUES(:name)")
+    suspend fun addFavorite(name: String)
+
+    // Elimina un pokemon de la tabla
+    @Query("DELETE FROM favorites WHERE pokemonName = :name")
+    suspend fun removeFavorite(name: String)
+
+    // Devuelve true o false dependiendo de si existe determinado pokemon en la BD
+    @Query("SELECT (SELECT COUNT(*) FROM favorites WHERE pokemonName = :name) == 1")
+    suspend fun isFavorite(name: String): Boolean
+
+
 }
