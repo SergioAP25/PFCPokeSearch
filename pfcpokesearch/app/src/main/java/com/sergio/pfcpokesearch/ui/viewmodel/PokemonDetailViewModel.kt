cd603ae2f3ccea3aea8fb39e52bf97583e761b28
Awoken Infinity <Sergio25Andrade50@gmail.com>
2023-05-29 09:29:52 +0200

    Finalizado PokemonDetailViewModel

--- pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/ui/viewmodel/PokemonDetailViewModel.kt
+++ pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/ui/viewmodel/PokemonDetailViewModel.kt
@@ -66,4 +66,67 @@ class PokemonDetailViewModel @Inject constructor(
             isLoading.postValue(false)
         }
     }
+
+    // Función encargada de obtener el pokemon seleccionado para la ventana de detalle y postearlo
+    fun homeFragmentCreate(){
+        scope = viewModelScope.launch {
+            isLoading.postValue(true)
+            // Llamada a la función randomPokemon
+            randomPokemon()
+            // Une el hilo que postea el pokemon random para a este
+            randomScope?.join()
+            // Llama al caso de uso para obtener la info del pokemon del detalle utilizando
+            // la variable pokemonName que es un live data en el que previamente se ha posteado
+            // el pokemon random
+            val result = getDetailPokemon(pokemonName)
+
+            pokemonModel.postValue(result)
+
+            // Obtención de la descripción en inglés ya que no siempre es la posición 0, además
+            // de limpiar el String de saltos de línea con los que viene la Api por defecto
+            var description = ""
+            var list = getPokemonDescriptions(pokemonName).descriptions
+            if(!list.isEmpty()){
+                for(i in 0 until list.size){
+                    if (list[i].language.name.equals("en")){
+                        description = getPokemonDescriptions(pokemonName).descriptions[i].flavor_text.replace("\n", " ")
+                        break
+                    }
+                }
+            }
+            else{
+                description = "This pokemon has no description known"
+            }
+
+            pokemonDescription.postValue(description)
+
+            isLoading.postValue(false)
+        }
+    }
+
+    // Función encargadada de obtener un pokemon aleatorio y postearlo en el live data
+    fun randomPokemon(){
+        randomScope = viewModelScope.launch {
+            isLoading.postValue(true)
+            val result = getRandomPokemon()
+            pokemonName = result.name
+            pokemonModel.postValue(result)
+            isLoading.postValue(false)
+        }
+    }
+
+    // Función encargada de llamar al caso de uso que añade favoritos a la base de datos
+    suspend fun addFavoritePokemon(name: String){
+        addFavorite(name)
+    }
+
+    // Función encargada de llamar al caso de uso que elimina favoritos de la base de datos
+    suspend fun removeFavoritePokemon(name: String){
+        removeFavorite(name)
+    }
+
+    // Función encargada de llamar al caso de uso que comprueba si un pokemon es favorito en la base de datos
+    suspend fun isFavoritePokemon(name: String): Boolean{
+        return isFavorite(name)
+    }
 }
