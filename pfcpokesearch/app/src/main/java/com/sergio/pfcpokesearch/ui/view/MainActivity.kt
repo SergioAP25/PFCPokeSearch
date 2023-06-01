7736645dea976932c2cbbec5051ab4b13fc9e457
Awoken Infinity <Sergio25Andrade50@gmail.com>
2023-06-01 22:11:15 +0200

    Añadido viewmodel a mainactivity y añadido override al método onresume para actualizar la base de datos

--- pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/ui/view/MainActivity.kt
+++ pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/ui/view/MainActivity.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.sergio.pfcpokesearch.R
 import com.sergio.pfcpokesearch.databinding.ActivityMainBinding
 import com.sergio.pfcpokesearch.ui.view.fragments.FavoriteFragment
 import com.sergio.pfcpokesearch.ui.view.fragments.HomeFragment
 import com.sergio.pfcpokesearch.ui.view.fragments.OptionsFragment
 import com.sergio.pfcpokesearch.ui.view.fragments.SearchFragment
+import com.sergio.pfcpokesearch.ui.viewmodel.PokemonViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 // Enum para cada tipo de proveedor de login
@@ -20,6 +22,8 @@ enum class ProviderType {
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    // Instancia del viewmodel
+    private lateinit var pokemonViewModel: PokemonViewModel
     private val homeFragment = HomeFragment()
     private val searchFragment = SearchFragment()
     private val favoriteFragment = FavoriteFragment()
@@ -29,6 +33,8 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // Asignación del viewmodel a la variable
+        pokemonViewModel = ViewModelProvider(this).get(PokemonViewModel::class.java)
 
         //Recoge los valores pasados por el intent
         val bundle = intent.extras
@@ -53,6 +59,21 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+    }
+
+    // Haciendo override de onResume asegura que la actualización a la base de datos solo
+    // se ejecute una vez el usuario se haya logeado y cada vez que la aplicación
+    // se reanude
+    override fun onResume() {
+        super.onResume()
+        // Si ya hay un hilo lo cancela
+        if(pokemonViewModel.updateScope!=null){
+            pokemonViewModel.updateScope!!.cancel()
+        }
+
+        // Llama a la función updatedatabase del viewmodel que se encarga
+        // de gestionar la actualización de la base de datos local
+        pokemonViewModel.updateDatabase()
     }
 
     // Función de conveniencia que recibe un fragment y reemplaza la vista actual por dicho fragment
