71ea698345c0a18c08503413b387d3c36b5c3bb3
Awoken Infinity <Sergio25Andrade50@gmail.com>
2023-06-01 22:04:09 +0200

    Añadida lógica a la FullScreenImageActivity

--- pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/ui/view/FullScreenImageActivity.kt
+++ pfcpokesearch/app/src/main/java/com/sergio/pfcpokesearch/ui/view/FullScreenImageActivity.kt
@@ -1,11 +1,23 @@
 package com.sergio.pfcpokesearch.ui.view
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sergio.pfcpokesearch.databinding.FullscreenImageActivityBinding
+import com.squareup.picasso.Picasso
 
 class FullScreenImageActivity: AppCompatActivity() {
     private lateinit var binding: FullscreenImageActivityBinding
     companion object {
         const val EXTRA_IMAGE = "extra_image"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FullscreenImageActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val image: String = intent.getStringExtra(FullScreenImageActivity.EXTRA_IMAGE).orEmpty()
+
+        Picasso.get().load(image).into(binding.pokemonImage)
     }
 }
